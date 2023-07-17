8c7caab55d9b546097618a15ac8e365d6f719d1b
Omochice <mochice.mls.ntl@gmail.com>
2023-07-17 22:08:33 +0900

    :muscle: split ripgrep match funcion and type

--- denops/ddu-source-anyjump/definitions.ts
+++ denops/ddu-source-anyjump/definitions.ts
@@ -6,40 +6,8 @@ import {
   getRgIgnoreSpecifier,
   REGEX_KEYWORD,
 } from "./base.ts";
-import {
-  $array,
-  $const,
-  $number,
-  $object,
-  $string,
-  type Infer,
-} from "https://esm.sh/lizod@0.2.6";
-
-/**
- * Validate is input ripgrep match object
- *
- * @param input validation target
- * @return Wheather is input matched
- */
-const validate = $object({
-  type: $const("match"),
-  data: $object({
-    path: $object({ text: $string }),
-    lines: $object({ text: $string }),
-    line_number: $number,
-    absolute_offset: $number,
-    submatches: $array(
-      $object({
-        match: $object({ text: $string }),
-        start: $number,
-        end: $number,
-      }),
-    ),
-  }),
-});
-
-export type Match = Infer<typeof validate>;
 import { decode } from "./decode.ts";
+import { type Match, validate } from "./ripgrepMatch.ts";
 
 /**
  * Search definitions by ripgrep
