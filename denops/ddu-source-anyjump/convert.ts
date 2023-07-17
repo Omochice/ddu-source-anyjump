a15e6ccfe16343e3463bcd44413b3864fc05e306
Omochice <mochice.mls.ntl@gmail.com>
2023-07-17 21:39:09 +0900

    :+1: Add ripgrep result converter
    from ddu-source-rg

---
+++ denops/ddu-source-anyjump/convert.ts
@@ -0,0 +1,64 @@
+import { Item } from "https://deno.land/x/ddu_vim@v3.4.2/types.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
+import { resolve } from "https://deno.land/std@0.194.0/path/mod.ts";
+import { type Match } from "./definitions.ts";
+
+/**
+ * Get length of UTF8 string
+ *
+ * @param str target string
+ * @return length of string
+ */
+const utf8Length = (() => {
+  const encoder = new TextEncoder();
+  return (str: string): number => encoder.encode(str).length;
+})();
+
+/**
+ * Convert ripgrep match to DDU's item
+ *
+ * @param match match element by ripgrep
+ * @param option convert option
+ * @return converted one
+ */
+export function convertMatch(match: Match, option: {
+  cwd: string;
+  hlGroupPath: string;
+  hlGroupLineNr: string;
+  hlGroupWord: string;
+}): Item<ActionData> {
+  const path = match.data.path.text;
+  const lineNr = match.data.line_number;
+  const col = match.data.submatches[0].start;
+  const header = `${path}:${lineNr}:${col}`;
+  const text = match.data.lines.text?.replace(/\r?\n/, "");
+  return {
+    word: header + text,
+    action: {
+      path: resolve(option.cwd, match.data.path.text),
+      lineNr,
+      col: col + 1,
+      text,
+    },
+    highlights: [
+      {
+        name: "path",
+        hl_group: option.hlGroupPath,
+        col: 1,
+        width: utf8Length(path),
+      },
+      {
+        name: "lineNr",
+        hl_group: option.hlGroupLineNr,
+        col: utf8Length(path) + 2,
+        width: utf8Length(lineNr.toString()),
+      },
+      {
+        name: "word",
+        hl_group: option.hlGroupWord,
+        col: utf8Length(header) + col + 1,
+        width: match.data.submatches[0].end - col,
+      },
+    ],
+  };
+}
