2435b2cafef984fbaa8b0af9823945b2d491f5cb
Omochice <44566328+Omochice@users.noreply.github.com>
2024-08-04 01:13:55 +0900

    test: use bdd module instead (#106)

---
+++ denops/ddu-source-anyjump/grepDefinitions.test.ts
@@ -0,0 +1,24 @@
+import { describe, it } from "jsr:@std/testing@1.0.0/bdd";
+import { expect } from "jsr:@std/expect@1.0.0";
+import { definitions } from "./grepDefinitions.ts";
+import { REGEX_KEYWORD } from "./ripgrep.ts";
+
+for (const [lang, definition] of definitions) {
+  describe(`Test for ${lang}`, () => {
+    for (const def of definition) {
+      const pattern = new RegExp(
+        def.pcre2Regexp.replaceAll(REGEX_KEYWORD, "test"),
+      );
+      it("should match", () => {
+        for (const spec of def.specSuccess) {
+          expect(spec).toMatch(pattern);
+        }
+      });
+      it("should NOT match", () => {
+        for (const spec of def.specFailed) {
+          expect(spec).not.toMatch(pattern);
+        }
+      });
+    }
+  });
+}
